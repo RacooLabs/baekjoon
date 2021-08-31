248da7989d7fbb0c8d8ccb47586933ae5921fc9f
Racoo <window340@gmail.com>
2021-08-31 14:57:47 +0900

    second commit

---
+++ Main.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int peopleNum = sc.nextInt();
+        int count = sc.nextInt();
+
+        int[] arr = new int[peopleNum];
+
+        for(int i=0;i<peopleNum;i++) {
+            arr[i] = i + 1;
+        }
+
+        int restPeople = peopleNum;
+
+        int currentCount = count-1;
+
+        System.out.print("<");
+
+        while(restPeople!=0){
+
+            if(restPeople == 1)
+                System.out.print(arr[currentCount]) ;
+            else
+                System.out.print(arr[currentCount]+", ");
+
+            arr[currentCount] = 0;
+
+            for(int i = 0 ; i<count; i++){
+                currentCount++;
+
+                currentCount %= peopleNum;
+
+                if(arr[currentCount] == 0){
+                    i--;
+                    if(restPeople == 1){
+                        break;
+                    }
+                }
+
+            }
+
+            restPeople--;
+
+        }
+
+        System.out.println(">");
+
+    }
+
+
+}
